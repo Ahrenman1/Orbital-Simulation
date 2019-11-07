f378a852f806c1b368ca4f6ae704ba7b8e7cabf6
Laurence Donkers <laurence.donkers@ororatech.com>
2019-11-07 12:47:14 +0000

    Ignore .asv, simplify Coord_generator

---
+++ Constellation_Generator.m
@@ -0,0 +1,7 @@
+%%Covsan is the TOP level script to preform simulation.
+
+a = 6.609e6;
+n = 4;
+in = 55;
+close all;
+Coord_Generator(a,0,in,0,0,0,n,a,0,in,0,60,0,n,a,0,in,0,120,0,n,a,0,in,0,180,0,n,a,0,in,0,240,0,n,a,0,in,0,300,0,n)
