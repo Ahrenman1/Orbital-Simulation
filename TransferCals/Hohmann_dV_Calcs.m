97db714585dc4b436a4047c7516c42daad66359f
duncan-wither <duncanwither@rocketmail.com>
2019-11-28 23:20:00 +0000

    Final Fix for dV and Mass Cals

--- TransferCals/Hohmann_dV_Calcs.m
+++ TransferCals/Hohmann_dV_Calcs.m
@@ -26,17 +26,24 @@ t_init=2*pi*sqrt(r_p.^3/mum);
 t_post=t_init*5/4;
 a_post=(mum*(t_post/(2*pi)).^2).^(1/3);
 r_spr = 2*a_post-r_p;%spreading orbit height 
+e_spr = (r_spr - r_p)/(r_spr + r_p);
+dV_spr_1 = sqrt((mum/a_post)*((1+e_spr)/(1-e_spr))) - sqrt(mum/r_p);
 
+%spreading orbits so they happen at the smame time
+t_orb_spr = t_init * 7/6;
+a_orb_spr=(mum*(t_orb_spr/(2*pi)).^2).^(1/3);
+ra_orb_spr = 2*a_orb_spr - r_p;
+e_orb_spr = (ra_orb_spr - r_p)/(ra_orb_spr + r_p);
+dV_orb_spr_1 = sqrt((mum/a_orb_spr)*((1+e_orb_spr)/(1-e_orb_spr))) - sqrt(mum/r_p);
 
 % Initilaising Matricies
-dV_esc = zeros(length(time_vals),1); 
+dV_esc = zeros(length(time_vals),1);        %dV 1
 transfer_time = zeros(length(time_vals),1);
 v_sat = zeros(length(time_vals),1);
-dV_inj = zeros(length(time_vals),1);
-dV_inc = zeros(length(time_vals),1);
-dV_spr = zeros(length(time_vals),1);
-dV_eccent = zeros(length(time_vals),1);
-
+dV_inj = zeros(length(time_vals),1);        %dV 2
+dV_orb_spr = zeros(length(time_vals),1);    %dV 3&4
+dV_inc = zeros(length(time_vals),1);        %dV 5
+dV_sat_spr = zeros(length(time_vals),1);     %dV 6&7
 
 %Calculating the dV required for each time interval
 for i = 1:length(time_vals)
@@ -62,15 +69,16 @@ for i = 1:length(time_vals)
     %injecting into parking orbit around mooon
     dV_inj(i) = sqrt((2*mum/r_p)+sqrt(abs(dV_capture))) - sqrt(mum/r_p);
     
+    %spreading orbits 60deg apart
+    dV_orb_spr(i) = 2*dV_orb_spr_1;
+    
     %changing inclination
     dV_inc(i) =  sqrt(2*v_orb.^2 - 2*cos(inc)*v_orb.^2);
     
     % making orbit eccentric to spread sattleites
-    % ahren plz add here!
-    dV_eccent(i) = sqrt((-mum/7.699e6)+(2*mum/r_p))-sqrt(mum/r_p);
-    dV_spread_1 = sqrt((2*mum/r_p)-(2*mum/(r_p+r_spr)))-sqrt(mum/r_spr);
-    dV_spr(i) = 2*dV_spread_1;%reverse kick is identical
+    dV_sat_spr(i) = 2*dV_spr_1;
     
+    %Calculaitn time left in loop
     elapsedT = toc(startT);
     remainingT = datestr(elapsedT*(length(time_vals)-i)/(60*60*24),'HH:MM:SS');
     fprintf('%s to go\n',remainingT);
@@ -84,15 +92,15 @@ figure(2)
 plot(time_vals,transfer_time/(60*60*24),'*')
 title('ToF to the Moon (Hohmann Transfer)')
 
-dV_tot = dV_esc + dV_inj + dV_inc + dV_spr;
+dV_tot = dV_esc + dV_inj + dV_inc + dV_sat_spr + dV_orb_spr;
 
 figure(3)
 plot(time_vals,dV_tot,'*')
 title('\DeltaV_{tot} to Desired Orbit (Hohmann Transfer)')
 
-save('dV_vals','dV_tot','dV_esc','dV_inj','dV_inc','dV_eccent','dV_spr')
+save('dV_vals','dV_tot','dV_esc','dV_inj','dV_inc','dV_orb_spr','dV_sat_spr')
 
 figure(4)
-plot(time_vals,dV_esc,'*',time_vals,dV_inj,'*',time_vals,dV_inc,'*',time_vals,dV_spr,'*',time_vals,dV_tot,'*')
+plot(time_vals,dV_esc,'*',time_vals,dV_inj,'*',time_vals,dV_orb_spr,'*',time_vals,dV_inc,'*',time_vals,dV_sat_spr,'*',time_vals,dV_tot,'*')
 title('\DeltaVs to Desired Orbit')
-legend('\DeltaV Escape','\DeltaV Inject','\DeltaV Incline','\DeltaV Spread','\DeltaV Total')
+legend('\DeltaV Escape','\DeltaV Inject','\DeltaV Spread Orbits','\DeltaV Incline','\DeltaV Spread Satelites','\DeltaV Total')
--- TransferCals/Mass_Calcs.m
+++ TransferCals/Mass_Calcs.m
@@ -19,13 +19,33 @@ fprintf('Delata 4 Payload To LEO Req = %6.0f Kg\n',Delta_4_M_init)
 Falcon_H_M_init = 24*prod(calc_K([348,348,348,348])).*200;
 fprintf('Falcon Heavy Payload To LEO Req = %6.0f Kg\n',Falcon_H_M_init)
 
+%%% Advanced optimised Calculations
+fprintf('More Advanced Calcs taking the fact not all sats need all the orbital, or satellite spreading impulses\n')';
+Delta_4_M_init_adv = (  1*prod(Delta_4_K([1,2,4]))+...
+                        3*prod(Delta_4_K([1,2,4,5]))+...
+                        5*prod(Delta_4_K(1:4))+...
+                        15*prod(Delta_4_K(1:5)))*200;
+
+                
+fprintf('Delata 4 Reduced Payload To LEO Req = %6.0f Kg\n',Delta_4_M_init_adv)
+FHK = calc_K([348,348,348,348]);
+Falcon_H_M_init_adv = ( 1*prod(FHK([1,2,4]))+...
+                        3*prod(FHK([1,2,4,5]))+...
+                        5*prod(FHK(1:4))+...
+                        15*prod(FHK(1:5)))*200;
+                
+fprintf('Falcon Heavy Reduced Payload To LEO Req = %6.0f Kg\n',Falcon_H_M_init_adv)
+
+fprintf('Percent Saving for Falcon Heavy = %3.0f \n',(1-(Falcon_H_M_init_adv/Falcon_H_M_init))*100)
+
 function K = calc_K(Is)
 load('dV_vals.mat');
 g0 = 9.81;
 K_1 = exp(max(dV_esc)./(Is(1).*g0)); %LEO -> MUN
 K_2 = exp(max(dV_inj)./(Is(2).*g0)); %MUN  CAPTURE
-K_3 = exp(max(dV_inc)./(Is(3).*g0)); %CHANGE INC
-K_4 = exp(max(dV_spr)./(Is(4).*g0)); %SEPERATE ON ORB
-K = [K_1, K_2, K_3, K_4];
+K_3 = exp(max(dV_orb_spr)./(Is(2).*g0)); %SPREAD ORBITS
+K_4 = exp(max(dV_inc)./(Is(3).*g0)); %CHANGE INC
+K_5 = exp(max(dV_sat_spr)./(Is(4).*g0)); %SPREAD SATTELITES
+K = [K_1, K_2, K_3, K_4, K_5];
 end
 
